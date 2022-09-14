746a484d251a7897f3bfee6e9a42f323d1e1883c
Demi Hayashi <demi.h@me.com>
2022-09-14 16:07:29 -0700

    complete set up of initial app architecture

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,18 @@
-// import sequelize & dotenv
+// import sequelize constructor from the library
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// create the connect to database
+// create connection to database
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialiect: 'mysql',
+        port: 3306
+    });
+};
+
+module.exports = sequelize;
