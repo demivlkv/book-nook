4fba266e67242f3cb30fc1ab14b90dee4143b94d
Demi Hayashi <demi.h@me.com>
2022-09-14 15:52:07 -0700

    set up server.js

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
-// import all npm packages
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
 
-// set up cookies
+const routes = require('../controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'secret garden',
+    cookie: {
+        // session expires after x minutes
+        
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
+app.use(routes);
 
 // turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+})
