7b700534fcfed0bf95158b3aff3c5c4c0d0c4c72
Sam Karp <sfcypret@gmail.com>
2022-09-15 16:56:19 -0400

    added comment to post route, updated index file to include models

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 // router.use((req, res) => {
 //   res.status(404).end();
 // });
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
 
 // GET request all posts
 router.get('/', (req, res) => {
     
     Post.findAll({
         //query config
-        attributes: ['id', 'post_url', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
+        attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
+            {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
             {
                 model: User, 
                 attributes: ['username']
