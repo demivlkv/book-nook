eeef6c4c1888ed6696d5fa1149691f50585a31d3
Sam Karp <sfcypret@gmail.com>
2022-09-15 17:01:06 -0400

    updated user and post routes with comments to view

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -39,6 +39,14 @@ router.get('/:id', (req, res) => {
         attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
             {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User, 
                 attributes: ['username']
             }
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // GET request for all /api/users
 router.get('/', (req, res) => {
@@ -20,8 +20,22 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
+    }, 
+    include: [
+      {
+        model: Post, 
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      }, 
+      //comment block
+      {
+        model: Comment, 
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+            model: Post, 
+            attributes: ['title']
+        }
     }
-    //insert models here 
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
