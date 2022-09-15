ef1ff473fa44f3737c1c7c3c11c84d0757a97ae7
Sam Karp <sfcypret@gmail.com>
2022-09-15 16:42:38 -0400

    comment model created

--- models/index.js
+++ models/index.js
@@ -1,8 +1,16 @@
 // create associations
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 //allows same user to post multiple comments
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,4 +18,4 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
