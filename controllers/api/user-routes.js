3f3b55110cb1ca235a69fe0c2f1a8d8fd41efd39
Sam Karp <sfcypret@gmail.com>
2022-09-16 16:05:48 -0400

    minor changes to server and user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { application } = require('express');
 const { User, Post, Comment } = require('../../models');
 const passport = require('passport');
 
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const bcrypt = require('bcrypt');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const { createBrotliCompress } = require('zlib');
 const { User } = require('./models');
 
 const app = express();
