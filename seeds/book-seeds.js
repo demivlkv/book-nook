816f63f977eec477e74aa5243b6012450137096d
Sam Karp <sfcypret@gmail.com>
2022-09-15 20:28:28 -0400

    book seeds created

---
+++ seeds/book-seeds.js
@@ -0,0 +1,22 @@
+const { Post } = require('../models');
+//template
+const postData = [
+    {
+        title: 
+        lexile_level:
+        genre: 
+        description:
+    }
+]
+
+//3rd grade - Sam
+
+
+
+//4th grade - Demi
+
+
+//5th grade - Mary
+
+
+//6th grade - Boris
