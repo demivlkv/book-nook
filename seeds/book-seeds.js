0b06519019dcd3bae83f0b0b66f6cc58d9a12955
Sam Karp <sfcypret@gmail.com>
2022-09-15 21:07:47 -0400

    3rd grade seeds added

--- seeds/book-seeds.js
+++ seeds/book-seeds.js
@@ -1,18 +1,44 @@
 const { Post } = require('../models');
 //template
 const postData = [
-    {
-        title: 
-        author: 
-        lexile_level:
-        genre: 
-        description:
-    }
-]
 
 //3rd grade - Sam
+{
+    title: 'Doctor De Soto',
+    author: 'William Steig', 
+    lexile_level: 560,
+    genre: 'Fiction',
+    description:"'Doctor De Soto, the dentist, did very good work.' With the aid of his able assistant, Mrs. De Soto, he copes with the toothaches of animals large and small. His expertise is so great that his fortunate patients never feel any pain. Since he's a mouse, Doctor De Soto refuses to treat 'dangerous' animals--that is, animals who have a taste for mice. But one day a fox shows up and begs for relief from the tooth that's killing him. How can the kindhearted De Sotos turn him away? But how can they make sure that the fox doesn't give in to his baser instincts once his tooth is fixed? Those clever De Sotos will find a way."
+}, 
+{
+    title: 'Weslandia', 
+    author: 'Paul Fleischman', 
+    lexile_level: 820, 
+    genre: 'Fiction', 
+    description: "Enter the witty, intriguing world of Weslandia! Now that school is over, Wesley needs a summer project. He's learned that each civilization needs a staple food crop, so he decides to sow a garden and start his own — civilization, that is. He turns over a plot of earth, and plants begin to grow. They soon tower above him and bear a curious-looking fruit. As Wesley experiments, he finds that the plant will provide food, clothing, shelter, and even recreation. It isn't long before his neighbors and classmates develop more than an idle curiosity about Wesley — and exactly how he is spending his summer vacation."
 
-
+}, 
+{
+    title: 'A Bad Case of Stripes', 
+    author: 'David Shannon', 
+    lexile_level: 610, 
+    genre: 'Fiction', 
+    description:"'What we have here is a bad case of stripes. One of the worst I've ever seen!' Camilla Cream loves lima beans, but she never eats them. Why? Because the other kids in her school don't like them. And Camilla Cream is very, very worried about what other people think of her. In fact, she's so worried that she's about to break out in...a bad case of stripes! 'Shannon's story is a good poke in the eye of conformity...and his empathetic, vivid artwork keeps perfect pace with the tale.'"
+}, 
+{
+    title: 'Runaway Ralph', 
+    author: 'Beverly Cleary', 
+    lexile_level: 890, 
+    genre: 'Fiction', 
+    description:"With a motorcycle to rev and the open road to see, Ralph S. Mouse is itching to run away from his overprotective family! But once he escapes to a summer campground nearby, the horrors of the wild make him doubt his plan. Angry cats, scary watchdogs, and grouchy gophers are only the half of it..."
+}, 
+{
+    title: 'Thunder Rose', 
+    author: 'Jerdine Nolen', 
+    lexile_level: 910, 
+    genre: 'Fiction', 
+    description:"Thunder Rose vows to grow up to be more than just big and strong, thank you very kindly--and boy, does she ever! But when a whirling storm on a riotous rampage threatens, has Rose finally met her match?"
+}, 
 
 //4th grade - Demi
 
@@ -21,3 +47,5 @@ const postData = [
 
 
 //6th grade - Boris
+
+]
