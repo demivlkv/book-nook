f8834176b7bf6bb6f22332256cbf65f9057f0ecd
Sam Karp <sfcypret@gmail.com>
2022-09-16 11:45:50 -0400

    minor changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path');
 
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -24,7 +25,6 @@ const sess = {
     })
 };
 
-// app.get('/', (req, res) => )
 
 app.use(session(sess));
 
