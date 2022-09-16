8dd2aaf44260ad66eb69bb300b3afae2b90ab16c
Demi Hayashi <demi.h@me.com>
2022-09-16 12:24:29 -0700

    fix run seed error & additional backend code

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,9 @@ const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/', homeRoutes);
+//router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+//router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
