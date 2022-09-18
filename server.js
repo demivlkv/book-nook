3b0451599a1f5c7b4b288d5522874bbfa3c98201
Sam Karp <sfcypret@gmail.com>
2022-09-18 17:16:02 -0400

    minor cleanup

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// if (process.env.NODE_ENV !== 'production') { require('dotenv').config() };
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
