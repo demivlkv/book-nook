351a69288568639a1d4e1c8b27ed69f95cf4576d
Sam Karp <sfcypret@gmail.com>
2022-09-18 14:28:35 -0400

    logout works, sign up and save session works, login adjusted

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,7 +61,14 @@ router.post('/signup', (req, res) => {
     username: req.body.username,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user = dbUserData;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+      res.json(dbUserData)});
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -107,13 +114,26 @@ router.post('/signup', (req, res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-     req.session.user = dbUserData
+    req.session.save(() => {
+      req.session.user = dbUserData;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
     // const test = ([dbUserData]).map(x => x)
     // console.log(JSON.stringify(dbUserData).split('id:').split(" ")[1]);
-            res.json({user: dbUserData});
-            
+      res.json({user: dbUserData});
+    });
           });
       })  
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+  req.session.destroy(() => {
+  res.status(204).end();
+  });
+  } else {
+          res.status(404).end();
+        }
+      });
+
 
 // PUT request to update user
 router.put('/:id', (req, res) => {
