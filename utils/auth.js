30107c238f3e45e42ad53b232d9323bd239bcfa5
Demi Hayashi <demi.h@me.com>
2022-09-20 13:36:24 -0700

    fix signup redirect bug

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // middleware for user authentication
 module.exports = function(req, res, next) {
-    if (req.session.user) {
+    if (req.session.user.id) {
         return next();
     }
     return res.redirect('/');
